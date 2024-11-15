7df8f748644353948f26001516e34b9ff13144a0
Valeria Mikhalevich <valeerriam@gmail.com>
2024-11-15 23:59:28 +0300

    Main Page ready + Architecture in process

--- script.js
+++ script.js
@@ -20,3 +20,18 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 //------------------------------------------------------------------------------
+
+document.querySelectorAll('.gallery-container img').forEach(img=>{
+    img.onclick = () =>{
+        document.querySelector('.pop-up').style.display = 'block';
+        document.querySelector('.pop-up img').src = img.getAttribute('src');
+     }
+});
+
+document.querySelector('.pop-up span').onclick = () =>{
+    document.querySelector('.pop-up').style.display = 'none';
+}
+
+//------------------------------------------------------------------------------
+
+
